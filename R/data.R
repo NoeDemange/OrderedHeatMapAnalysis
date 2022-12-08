8d1cbba7f976d60749f34fd076ccef923ea1ea1e
noé demange <demange.noe@outlook.fr>
2022-12-08 07:49:49 +0100

    Ajout de mot dans Wordlist pour passer check

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Matrix Folds/species
 #'
-#' Jeu de donnees biologique.
+#' biological dataset.
 #'
 #' @format A data frame with 1072 rows and 210 variables
 #' @source <https://doi.org/10.1111/evo.14550>
